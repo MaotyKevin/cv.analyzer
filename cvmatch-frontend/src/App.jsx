e767d63f9f28a45ec93487febe0b57cc034b5b15
MaotyKevin <maotykevin@gmail.com>
2026-06-06 00:14:37 +0300

    auth and hooks

--- cvmatch-frontend/src/App.jsx
+++ cvmatch-frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import InputForm from "./components/InputForm";
 import ResultPage from "./components/ResultPage";
 import AuthPage from "./components/AuthPage";
